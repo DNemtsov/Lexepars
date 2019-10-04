44b88e535f2ba31a3ee4dce769127905a41708d2
Dmitrii Nemtsov <asssvk@gmail.com>
2019-10-03 20:32:01 -0700

    Minor refactoring of some extension methods.

--- src/Lexepars/ParserExtensions.cs
+++ src/Lexepars/ParserExtensions.cs
@@ -3,40 +3,60 @@ using System;
 
 namespace Lexepars
 {
+    /// <summary>
+    /// Extension methods for the <see cref="IParser{TValue}"/>.
+    /// </summary>
     public static class ParserExtensions
     {
         /// <summary>
         /// Allows LINQ syntax to construct a new parser from a simpler parser, using a single 'from' clause.
         /// </summary>
-        public static IParser<TResult> Select<TInterim, TResult>(this IParser<TInterim> parser, Func<TInterim, TResult> resultContinuation)
-        {
-            return new MonadicBindParser<TInterim, TResult>(parser, resultContinuation);
-        }
+        /// <typeparam name="TInterim">The type of the interim parsed result.</typeparam>
+        /// <typeparam name="TValue">The type of the final parsed result.</typeparam>
+        /// <param name="interimParser">The interim parser. Not null.</param>
+        /// <param name="resultMapping">Function mapping the interim parsing result to the final parsing result. Not null.</param>
+        public static IParser<TValue> Select<TInterim, TValue>(this IParser<TInterim> interimParser, Func<TInterim, TValue> resultMapping) => new MonadicBindParser<TInterim, TValue>(interimParser, resultMapping);
 
         /// <summary>
         /// Allows LINQ syntax to contruct a new parser from an ordered sequence of simpler parsers, using multiple 'from' clauses.
         /// </summary>
-        public static IParser<TResult> SelectMany<T1, T2, TResult>(this IParser<T1> parser, Func<T1, IParser<T2>> value1ToParser2Continuation, Func<T1, T2, TResult> resultContinuation)
-        {
-            return new MonadicBindParser<T1, T2, TResult>(parser, value1ToParser2Continuation, resultContinuation);
-        }
+        /// <typeparam name="TInterim1">The type of the first interim parsed result.</typeparam>
+        /// <typeparam name="TInterim2">The type of the second interim parsed result.</typeparam>
+        /// <typeparam name="TValue">The type of the final parsed result.</typeparam>
+        /// <param name="interimParser1">First interim parser. Not null.</param>
+        /// <param name="result1ToParser2Mapping">Function mapping the first interim parsing result to the second interim parser. Not null.</param>
+        /// <param name="resultMapping">Function mapping the interim parsing result to the final parsing result. Not null.</param>
+        public static IParser<TValue> SelectMany<TInterim1, TInterim2, TValue>(this IParser<TInterim1> interimParser1, Func<TInterim1, IParser<TInterim2>> result1ToParser2Mapping, Func<TInterim1, TInterim2, TValue> resultMapping) => new MonadicBindParser<TInterim1, TInterim2, TValue>(interimParser1, result1ToParser2Mapping, resultMapping);
 
         /// <summary>
-        /// Allows to bind a mapping function to the parser reply.
+        /// Binds a result-mapping function to the `interimParser.
         /// </summary>
-        public static IParser<TResult> Bind<TInterim, TResult>(this IParser<TInterim> parser, Func<TInterim, TResult> resultContinuation)
-        {
-            return new MonadicBindParser<TInterim, TResult>(parser, resultContinuation);
-        }
+        /// <typeparam name="TInterim">The type of the interim parsed result.</typeparam>
+        /// <typeparam name="TValue">The type of the final parsed result.</typeparam>
+        /// <param name="interimParser">The interim parser. Not null.</param>
+        /// <param name="resultMapping">Function mapping the interim parsing result to the final parsing result. Not null.</param>
+        public static MonadicBindParser<TInterim, TValue> Bind<TInterim, TValue>(this IParser<TInterim> interimParser, Func<TInterim, TValue> resultMapping) => new MonadicBindParser<TInterim, TValue>(interimParser, resultMapping);
 
-        public static IParser<TResult> ThenSkip<TResult>(this IParser<TResult> item, IGeneralParser following)
-        {
-            return new TakeSkipParser<TResult>(item, following);
-        }
+        [Obsolete(message: "Use TakeAndThenSkip instead.")]
+        public static IParser<TValue> ThenSkip<TValue>(this IParser<TValue> item, IGeneralParser following) => new TakeSkipParser<TValue>(item, following);
 
-        public static IParser<TResult> Take<TResult>(this IGeneralParser preceding, IParser<TResult> item)
-        {
-            return new SkipTakeParser<TResult>(preceding, item);
-        }
+        /// <summary>
+        /// Takes the `item and then skips the `skip.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the parsed result.</typeparam>
+        /// <param name="item">The item parser. Not null.</param>
+        /// <param name="skip">The parser of the part to be skipped. Not null.</param>
+        public static IParser<TValue> TakeAndThenSkip<TValue>(this IParser<TValue> item, IGeneralParser skip) => new TakeSkipParser<TValue>(item, skip);
+
+        [Obsolete(message: "Use SkipAndThenTake instead.")]
+        public static IParser<TValue> Take<TValue>(this IGeneralParser preceding, IParser<TValue> item) => new SkipTakeParser<TValue>(preceding, item);
+
+        /// <summary>
+        /// Skips the `skip and then takes the `item.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the parsed result.</typeparam>
+        /// <param name="skip">The parser of the part to be skipped. Not null.</param>
+        /// <param name="item">The item parser. Not null.</param>
+        public static SkipTakeParser<TValue> SkipAndThenTake<TValue>(this IGeneralParser skip, IParser<TValue> item) => new SkipTakeParser<TValue>(skip, item);
     }
 }
--- src/Lexepars/Token/TokenKindExtensions.cs
+++ src/Lexepars/Token/TokenKindExtensions.cs
@@ -3,14 +3,18 @@ using Lexepars.Parsers;
 
 namespace Lexepars
 {
+    /// <summary>
+    /// Extension methods for the <see cref="TokenKind"/>.
+    /// </summary>
     public static class TokenKindExtensions
     {
         /// <summary>
-        /// Creates a <see cref="ConstantParser"/>.
+        /// Creates a <see cref="ConstantParser{TValue}"/>.
         /// </summary>
         /// <typeparam name="TValue">The type of the parsed result.</typeparam>
+        /// <param name="tokenKind">The kind of token. Not null.</param>
         /// <param name="constant">The constant value to be returned.</param>
-        /// <returns>The new instance of <see cref="ConstantParser"/>. Not null.</returns>
+        /// <returns>The new instance of <see cref="ConstantParser{TValue}"/>. Not null.</returns>
         public static ConstantParser<TValue> Constant<TValue>(this TokenKind tokenKind, TValue constant) => new ConstantParser<TValue>(tokenKind, constant);
 
         /// <summary>
@@ -31,5 +35,12 @@ namespace Lexepars
         /// <typeparam name="TValue">The type of the parsed result.</typeparam>
         /// <returns>The new instance of <see cref="BindTokenLexemeByKindParser{TValue}"/>. Not null.</returns>
         public static BindTokenLexemeByKindParser<TValue> BindLexeme<TValue>(this TokenKind kind, Func<string, TValue> resultContinuation) => new BindTokenLexemeByKindParser<TValue>(kind, resultContinuation);
+
+        /// <summary>
+        /// Creates a <see cref="SkipTakeParser{TValue}"/> skipping the specified-kind token of the and then taking the `item.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the parsed result.</typeparam>
+        /// <returns>The new instance of <see cref="SkipTakeParser{TValue}"/>. Not null.</returns>
+        public static SkipTakeParser<TValue> SkipThenTake<TValue>(this TokenKind tokenKind, IParser<TValue> item) => new SkipTakeParser<TValue>(new TokenByKindParser(tokenKind), item);
     }
 }
